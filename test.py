fc2d5d3fee266484f26661622f5aee3baa057316
huang <1021098080@qq.com>
2024-02-26 23:18:58 +0800

    Add index page

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
 
+name = 'Mr huang'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello():
-    return 'Welcome to Mypage!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/home/<name>')
 def user_page(name):
